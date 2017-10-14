96aa9463c77cea7c6482726f69940aeab3d67a52
I3eing <Leherketa@gmail.com>
2017-10-14 20:32:46 +0700

    change token

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'kc3ZUoxL3mD+tconhFmlnYeopbrp/Vzjy/x7JVRDLHCtNHfmSCRY8+Ec35dkGzJcVo8Ooy+HAnExZLfxUiwlyIYuvLNv2yFN86xlILFIKAw0z09nhIjGdWt9FYJlPEgalPWDRr/G5EKxdTElsXzlwQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'nHDUCFtdaY7TRP+0G0GIQLXWyhc5Ga0lTnR0nkizkgTyd47iJhi3O6IrdTznVMgfVo8Ooy+HAnExZLfxUiwlyIYuvLNv2yFN86xlILFIKAwVv8ojRWEJEPIeaWKKP2bbjLXQgyyb2mOKDFXEZcpzCwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
